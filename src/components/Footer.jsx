975def5a8a861f92516c691fa7a0d43de613d39c
Amanprajapati145 <ak8292333@gmail.com>
2025-11-08 14:33:56 +0530

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "../context/useAuth.jsx";
-// import { FaGithub, FaLinkedin, FaTwitter, FaEnvelope } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 
 const Footer = () => {
   const { darkMode } = useAuth();
